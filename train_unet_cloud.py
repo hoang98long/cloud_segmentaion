5523311d9fdbefa492c26e498e9f37f44a0542b1
hoang98long <53008046+hoang98long@users.noreply.github.com>
2024-05-23 15:19:29 +0700

    commit

--- train_unet_cloud.py
+++ train_unet_cloud.py
@@ -5,12 +5,107 @@ from torch.utils.data import Dataset, DataLoader, sampler
 from PIL import Image
 import torch
 import time
+from torch import nn
+from IPython.display import clear_output
 from utils.dataset import CloudDataset
+from utils.model import UNET
 
 base_path = Path('./input/38-Cloud_training')
-data = CloudDataset(base_path/'train_red',
-                    base_path/'train_green',
-                    base_path/'train_blue',
-                    base_path/'train_nir',
-                    base_path/'train_gt')
-print(len(data))
+data = CloudDataset(base_path / 'train_red',
+                    base_path / 'train_green',
+                    base_path / 'train_blue',
+                    base_path / 'train_nir',
+                    base_path / 'train_gt')
+# print(len(data))
+train_ds, valid_ds = torch.utils.data.random_split(data, (6000, 2400))
+train_dl = DataLoader(train_ds, batch_size=12, shuffle=True)
+valid_dl = DataLoader(valid_ds, batch_size=12, shuffle=True)
+xb, yb = next(iter(train_dl))
+unet = UNET(4, 2)
+
+
+def train(model, train_dl, valid_dl, loss_fn, optimizer, acc_fn, epochs=1):
+    start = time.time()
+    model.cuda()
+
+    train_loss, valid_loss = [], []
+
+    best_acc = 0.0
+
+    for epoch in range(epochs):
+        print('Epoch {}/{}'.format(epoch, epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train(True)  # Set trainind mode = true
+                dataloader = train_dl
+            else:
+                model.train(False)  # Set model to evaluate mode
+                dataloader = valid_dl
+
+            running_loss = 0.0
+            running_acc = 0.0
+
+            step = 0
+
+            # iterate over data
+            for x, y in dataloader:
+                x = x.cuda()
+                y = y.cuda()
+                step += 1
+
+                # forward pass
+                if phase == 'train':
+                    # zero the gradients
+                    optimizer.zero_grad()
+                    outputs = model(x)
+                    loss = loss_fn(outputs, y)
+
+                    # the backward pass frees the graph memory, so there is no
+                    # need for torch.no_grad in this training pass
+                    loss.backward()
+                    optimizer.step()
+                    # scheduler.step()
+
+                else:
+                    with torch.no_grad():
+                        outputs = model(x)
+                        loss = loss_fn(outputs, y.long())
+
+                # stats - whatever is the phase
+                acc = acc_fn(outputs, y)
+
+                running_acc += acc * dataloader.batch_size
+                running_loss += loss * dataloader.batch_size
+
+                if step % 100 == 0:
+                    # clear_output(wait=True)
+                    print('Current step: {}  Loss: {}  Acc: {}  AllocMem (Mb): {}'.format(step, loss, acc,
+                                                                                          torch.cuda.memory_allocated() / 1024 / 1024))
+                    # print(torch.cuda.memory_summary())
+
+            epoch_loss = running_loss / len(dataloader.dataset)
+            epoch_acc = running_acc / len(dataloader.dataset)
+
+            clear_output(wait=True)
+            print('Epoch {}/{}'.format(epoch, epochs - 1))
+            print('-' * 10)
+            print('{} Loss: {:.4f} Acc: {}'.format(phase, epoch_loss, epoch_acc))
+            print('-' * 10)
+
+            train_loss.append(epoch_loss) if phase == 'train' else valid_loss.append(epoch_loss)
+
+    time_elapsed = time.time() - start
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return train_loss, valid_loss
+
+
+def acc_metric(predb, yb):
+    return (predb.argmax(dim=1) == yb.cuda()).float().mean()
+
+
+loss_fn = nn.CrossEntropyLoss()
+opt = torch.optim.Adam(unet.parameters(), lr=0.01)
+train_loss, valid_loss = train(unet, train_dl, valid_dl, loss_fn, opt, acc_metric, epochs=50)
