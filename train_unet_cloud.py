bd67402e20eeab4e5a39c3172315acf342093cdc
hoang98long <53008046+hoang98long@users.noreply.github.com>
2024-05-23 14:37:31 +0700

    Add files via upload

---
+++ train_unet_cloud.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader, sampler
+from PIL import Image
+import torch
+import time
+from utils.dataset import CloudDataset
+
+base_path = Path('../input/38cloud-cloud-segmentation-in-satellite-images/38-Cloud_training')
+data = CloudDataset(base_path/'train_red',
+                    base_path/'train_green',
+                    base_path/'train_blue',
+                    base_path/'train_nir',
+                    base_path/'train_gt')
+print(len(data))
